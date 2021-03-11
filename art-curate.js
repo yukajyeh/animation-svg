e85d2aa003e3ab421a0efb76718c090eac5048af
Yuka Yeh <yukaka210@gmail.com>
2021-03-11 22:59:58 +0100

    raw finish

--- art-curate.js
+++ art-curate.js
@@ -1,4 +1,8 @@
 const headerTag = document.querySelector("header")
+const blobGroups = document.querySelectorAll("g.blob")
+const sectionTags = document.querySelectorAll("section")
+const arrowTag = document.querySelector("arrow")
+
 
 const easing = function(x) {
     return x * x * x
@@ -7,9 +11,33 @@ const easing = function(x) {
 const fadeHeader = function(){
     const pixels = window.pageYOffset
 
-    headerTag.style.opacity = 1 - easing(pixels / 500)
+    headerTag.style.opacity = 1 - easing(pixels / 400)
+}
+
+const fadeArrow = function(){
+    const pixels = window.pageYOffset
+    arrowTag.style.opacity = 1 - easing (pixels / 400)
+}
+
+const checkBlobs = function() {
+
+    const pixels = window.pageYOffset
+
+    blobGroups.forEach((blob , index) => {
+        const currentSection = sectionTags[index]
+
+        if(pixels > currentSection.offsetTop - 300){
+            blob.classList.add("in-view")
+        } else {
+            blob.classList.remove("in-view")
+        }
+    })
 }
 
 window.addEventListener("scroll", function(){
     fadeHeader()
+   
+    checkBlobs()
 })
+
+
